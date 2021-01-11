6f0b8284ae8e56d6165ae499a6b0b6a8f6acb0f9
Syed Afrid Ali <aliatif908@gmail.com>
2021-01-11 18:31:28 +0530

    minor refactor

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Static front page template
+ *
+ * @package dsignfly
+ */
+
+get_header();
+?>
+
+<?php
+get_footer();
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package d\'signfly
+ * @package dsignfly
  */
 
 get_header();
