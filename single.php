e34488f1a151fa7f5dda66ec97b09f6a4e07c52b
Syed Afrid Ali <aliatif908@gmail.com>
2021-01-15 15:54:28 +0530

    Comment form

--- single.php
+++ single.php
@@ -18,16 +18,13 @@ get_header();
 
 				get_template_part( 'template-parts/content', get_post_type() );
 
-				the_post_navigation(
-					array(
-						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'dsignfly' ) . '</span> <span class="nav-title">%title</span>',
-						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'dsignfly' ) . '</span> <span class="nav-title">%title</span>',
-					)
-				);
-
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
+				if ( comments_open() || get_comments_number() ) : ?>
+					<div class="comments-wrapper">
+						<p class="bars"><?php esc_html_e( 'Comments', 'designfly' ); ?></p>
+					<?php comments_template(); ?>
+					</div>
+				<?php
 				endif;
 
 			endwhile; // End of the loop.
