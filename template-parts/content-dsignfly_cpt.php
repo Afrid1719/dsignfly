1a3fd1d7126f413b012d4879032972c4a87d23c6
Syed Afrid Ali <aliatif908@gmail.com>
2021-01-19 17:06:32 +0530

    Tags added on Single Post Page, Few UI fixes

--- template-parts/content-dsignfly_cpt.php
+++ template-parts/content-dsignfly_cpt.php
@@ -15,3 +15,18 @@
     </div>
 </header>
 <article class="dsignfly-single-post-content"><?php the_content(); ?></article>
+<div class="associated-tags">
+    <?php 
+    $tags = get_the_tags();
+
+    if (!! $tags): ?>
+        TAGS: 
+        <?php for($i = 0; $i < count($tags); $i++): ?>
+            <a class="tag" href="<?php echo get_term_link($tags[$i]); ?>"><?php echo $tags[$i]->name; ?></a>
+            <?php echo ($i !== count($tags) - 1) ? ',' : '';?>
+        <?php endfor; ?>
+    <?php
+    endif;
+    ?>
+
+</div>
